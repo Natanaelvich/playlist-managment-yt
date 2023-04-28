43912a73bb9530001b7becc327780571cd2afe5e
Natanaelvich <taelima1997@gmail.com>
2023-04-27 23:26:34 -0300

    remoce signin implementation

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,20 +1,4 @@
-import { OAuth2Client } from "google-auth-library";
 import axios from "axios";
-
-const API_KEY = "YOUR_API_KEY";
-const CLIENT_ID = "YOUR_CLIENT_ID";
-const SCOPE = "https://www.googleapis.com/auth/youtube.readonly";
-
-const client = new OAuth2Client(CLIENT_ID);
-
-export async function signIn() {
-  try {
-    const url = client.generateAuthUrl({ access_type: "offline", scope: SCOPE });
-    window.location.assign(url);
-  } catch (error) {
-    console.error("Failed to sign in:", error);
-  }
-}
 
 export async function fetchMyPlaylists(accessToken: string) {
   try {
@@ -26,7 +10,6 @@ export async function fetchMyPlaylists(accessToken: string) {
         part: "snippet",
         mine: true,
         maxResults: 50,
-        key: API_KEY,
       },
     });
     return response.data.items;
