ea370d8d25a42fd20fa1549a0f2514fa06bceec2
Natanaelvich <taelima1997@gmail.com>
2023-04-28 23:35:47 -0300

    update SignInWithGooglePage

--- src/pages/SignInWithGoogle.tsx
+++ src/pages/SignInWithGoogle.tsx
@@ -1,23 +1,29 @@
-import { CredentialResponse, GoogleLogin } from "@react-oauth/google";
+import { useGoogleLogin } from "@react-oauth/google";
+import { useTokenUserStore } from "../store/tokenUserSlice";
 
 const SignInWithGoogle = () => {
-  const responseMessage = (response: CredentialResponse) => {
-    console.log(response);
-  };
+  const { setToken } = useTokenUserStore();
 
-  const errorMessage = () => {
-    console.log("error");
-  };
+  const login = useGoogleLogin({
+    flow : 'implicit',
+    onSuccess: (tokenResponse) => setToken(tokenResponse.access_token),
+    scope: "https://www.googleapis.com/auth/youtube.readonly",
+  });
 
   return (
-    <div className="bg-gradient-to-r from-purple-900 to-purple-500 h-screen flex flex-col justify-center">
-      <div className="text-center">
-        <h1 className="text-4xl text-white font-bold mb-6">Playlist Manager</h1>
+    <div className="bg-gradient-to-r from-purple-900 to-purple-500 h-screen flex flex-col justify-center items-center">
+      <h1 className="text-4xl text-white font-bold mb-6">Playlist Manager</h1>
 
-        <div className="flex justify-center">
-          <GoogleLogin onSuccess={responseMessage} onError={errorMessage} />
-        </div>
-      </div>
+      <button
+        className="bg-white text-purple-900 px-4 py-2 rounded-lg shadow-lg flex"
+        onClick={() => login()}
+      >
+        <img
+          className="w-6 h-6 mr-2"
+          src="https://img.icons8.com/color/48/000000/google-logo.png"
+        />
+        Login with Google
+      </button>
 
       <footer className="text-center text-white mt-6">
         <p>
