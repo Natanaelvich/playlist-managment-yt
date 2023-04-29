46038fc3193bcc7fffca3135de0dfc9764c11bb1
Natanaelvich <taelima1997@gmail.com>
2023-04-28 23:46:19 -0300

    update with implementation PlaylistVideos

--- src/pages/PlayLists.tsx
+++ src/pages/PlayLists.tsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
+import PlaylistVideos from "./PlaylistVideos";
 
 type Playlist = {
   kind: string;
@@ -51,6 +52,9 @@ type Props = {
 
 const PlayLists = ({ accessToken }: Props) => {
   const [playlists, setPlaylists] = useState<Playlist[]>([]);
+  const [selectedPlaylistId, setSelectedPlaylistId] = useState<string | null>(
+    null
+  );
 
   useEffect(() => {
     const fetchPlaylists = async () => {
@@ -76,39 +80,58 @@ const PlayLists = ({ accessToken }: Props) => {
     fetchPlaylists();
   }, [accessToken]);
 
+  const handlePlaylistClick = (playlistId: string) => {
+    setSelectedPlaylistId(playlistId);
+  };
+
+  const handleClosePlaylist = () => {
+    setSelectedPlaylistId(null);
+  };
+
   return (
     <div className="py-6 bg-gray-900 text-gray-200 h-screen">
       <div className="container mx-auto px-4">
-        <h1 className="text-3xl font-bold mb-6">My Playlists</h1>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          {playlists.map((playlist) => (
-            <div
-              key={playlist.id}
-              className="rounded-lg shadow-md overflow-hidden bg-gray-800"
-            >
-              <img
-                src={playlist.snippet.thumbnails.medium.url}
-                alt={playlist.snippet.title}
-                className="w-full h-48 object-cover"
-              />
-              <div className="p-6">
-                <div className="flex items-center mb-4">
-                  <h2 className="text-lg font-medium">
-                    {playlist.snippet.title}
-                  </h2>
-                </div>
-                <a
-                  href={`https://www.youtube.com/playlist?list=${playlist.id}`}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-md text-sm font-medium"
+        {selectedPlaylistId ? (
+          <PlaylistVideos
+            accessToken={accessToken}
+            playlistId={selectedPlaylistId}
+            onClose={handleClosePlaylist}
+          />
+        ) : (
+          <>
+            <h1 className="text-3xl font-bold mb-6">My Playlists</h1>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+              {playlists.map((playlist) => (
+                <div
+                  key={playlist.id}
+                  className="rounded-lg shadow-md overflow-hidden bg-gray-800"
+                  onClick={() => handlePlaylistClick(playlist.id)}
                 >
-                  Open Playlist
-                </a>
-              </div>
+                  <img
+                    src={playlist.snippet.thumbnails.medium.url}
+                    alt={playlist.snippet.title}
+                    className="w-full h-48 object-cover"
+                  />
+                  <div className="p-6">
+                    <div className="flex items-center mb-4">
+                      <h2 className="text-lg font-medium">
+                        {playlist.snippet.title}
+                      </h2>
+                    </div>
+                    <a
+                      href={`https://www.youtube.com/playlist?list=${playlist.id}`}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-md text-sm font-medium"
+                    >
+                      Open Playlist
+                    </a>
+                  </div>
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
+          </>
+        )}
       </div>
     </div>
   );
