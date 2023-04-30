c5a838110dd9df65b92c02b2183713b87b472e0e
Natanaelvich <taelima1997@gmail.com>
2023-04-29 22:48:23 -0300

    update main with routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import Routes from "./routes.tsx";
 
 const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <GoogleOAuthProvider clientId={clientId}>
     <React.StrictMode>
-      <App />
+      <Routes />
     </React.StrictMode>
   </GoogleOAuthProvider>
 );
