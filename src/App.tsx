2878715ece4817fa05c2d54e1abafbd8c74a93ac
Natanaelvich <taelima1997@gmail.com>
2023-04-29 22:49:24 -0300

    add get data storage on load

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,33 @@
+import { useCallback, useEffect, useState } from "react";
 import PlayLists from "./pages/PlayLists";
 import SignInWithGoogle from "./pages/SignInWithGoogle";
 import { useTokenUserStore } from "./store/tokenUserSlice";
 import "./styles/main.css";
 
 function App() {
-  const { token } = useTokenUserStore();
+  const { token, setToken } = useTokenUserStore();
+
+  const [loading, setLoading] = useState(true);
+
+  const loadToken = useCallback(() => {
+    setLoading(true);
+    const token = localStorage.getItem("token");
+    if (token) {
+      setToken(token);
+    }
+    setLoading(false);
+  }, [setToken]);
+
+  useEffect(() => {
+    loadToken();
+  }, [loadToken]);
+
+  if (loading) {
+    return null;
+  }
 
   return (
-    <>
-      {token ? (
-        <PlayLists accessToken={token} />
-      ) : (
-        <SignInWithGoogle />
-      )}
-    </>
+    <>{token ? <PlayLists accessToken={token} /> : <SignInWithGoogle />}</>
   );
 }
 
